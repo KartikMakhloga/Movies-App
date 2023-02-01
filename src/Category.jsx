bda86317887c9e7551b17c196ee9a3311f676d56
kartik <kartikmakhloga20@gmail.com>
2023-02-01 12:34:11 +0530

    connect app with movies-api

--- src/Category.jsx
+++ src/Category.jsx
@@ -2,14 +2,24 @@ import React from "react";
 
 class Category extends React.Component {
   state = {
-    allGenre: ["Action", "Comedy", "Romance", "Thriller", "Horrer"],
+    allGenre: [],
   };
+
+  componentDidMount(){
+    //API call(msg bhejna => get)
+
+    fetch("/genre").then(function(res){
+      return res.json()
+    }).then((json)=>{
+      this.setState({allGenre:json})
+    })
+  }
 
   render() {
     return (
       <ul class="list-group">
         {this.state.allGenre.map((el) => {
-          return <li class="list-group-item">{el}</li>;
+          return <li class="list-group-item" key={el._id}>{el.name}</li>;
         })}
       </ul>
     );
